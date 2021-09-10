633c7b5947d02dc1119a6d09d3dbcf245f9671a3
Serhii Tomashkevich <19tomat@gmail.com>
2021-09-10 14:09:46 +0300

    ~update: action object add case

--- src/contacts.js
+++ src/contacts.js
@@ -1,32 +1,53 @@
 const fs = require("fs/promises");
 const path = require("path");
 const crypto = require("crypto");
+const { exit } = require("process");
 
 const contactsPath = path.join(__dirname, "db/contacts.json");
 
 async function listContacts() {
-  const file = await fs.readFile(contactsPath);
-  return JSON.parse(file);
+  try {
+    const file = await fs.readFile(contactsPath);
+    return JSON.parse(file);
+  } catch (error) {
+    console.warn(error.message);
+    exit(1);
+  }
 }
 
 async function getContactById(contactId) {
-  const contacts = await listContacts();
-  return contacts.filter((el) => el.id.toString() === contactId);
+  try {
+    const contacts = await listContacts();
+    return contacts.filter((el) => el.id.toString() === contactId);
+  } catch (error) {
+    console.warn(error.message);
+    exit(1);
+  }
 }
 
 async function removeContact(contactId) {
-  const contacts = await listContacts();
-  const contactWithoutRemoved = contacts.filter((el) => el.id.toString() !== contactId);
-  await fs.writeFile(contactsPath, JSON.stringify(contactWithoutRemoved));
-  return await listContacts();
+  try {
+    const contacts = await listContacts();
+    const contactWithoutRemoved = contacts.filter((el) => el.id.toString() !== contactId);
+    await fs.writeFile(contactsPath, JSON.stringify(contactWithoutRemoved));
+    return await listContacts();
+  } catch (error) {
+    console.warn(error.message);
+    exit(1);
+  }
 }
 
-async function addContact(name, email, phone) {
-  const contacts = await listContacts();
-  const id = crypto.randomUUID({ disableEntropyCache: false });
-  contacts.push({ name, email, phone, id });
-  await fs.writeFile(contactsPath, JSON.stringify(contacts));
-  return await listContacts();
+async function addContact({ name, email, phone }) {
+  try {
+    const contacts = await listContacts();
+    const id = crypto.randomUUID({ disableEntropyCache: false });
+    contacts.push({ name, email, phone, id });
+    await fs.writeFile(contactsPath, JSON.stringify(contacts));
+    return await listContacts();
+  } catch (error) {
+    console.warn(error.message);
+    exit(1);
+  }
 }
 
 module.exports = { contactsPath, listContacts, getContactById, removeContact, addContact };
